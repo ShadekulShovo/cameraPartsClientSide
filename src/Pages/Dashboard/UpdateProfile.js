a9d64ebc074ae1706f0c85091a7ba348cebb418f
Shadekul Shovo <shadekuljamalpur.bd21@gmail.com>
2022-05-26 17:59:47 +0600

    Fix

--- src/Pages/Dashboard/UpdateProfile.js
+++ src/Pages/Dashboard/UpdateProfile.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
 const UpdateProfile = () => {
-    return (
-        <div>
-            <h1> update profile</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Profile Update</h1>
+    </div>
+  );
 };
 
 export default UpdateProfile;
