bfc9a208a55515bd6ec9dd459ff403e3be514262
Shadekul Shovo <shadekuljamalpur.bd21@gmail.com>
2022-05-26 17:31:04 +0600

    Fix

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -7,10 +7,10 @@ const Banner = () => {
         <div class="hero-content flex-col lg:flex-row-reverse">
           <img src="https://i.ibb.co/XJK5tC1/pexels-pixabay-51383-removebg-preview.png" />
           <div className=" max-w-3xl">
-            <h1 class="text-5xl font-bold">
-              Camera PARTS <br />
-              FOR ALL OF Photo Magnet <br /> PARTAS{" "}
-            </h1>
+            <h1 class="text-5xl font-bold">Camera Parts </h1>
+            <h2 class="text-3xl">For All Of Photography Enthusiast.</h2>
+            <h3 class="text-2xl font-bold">Choose Which PARTAS you NEED!!!</h3>
+
             <p class="py-6">
               Lorem, ipsum dolor sit amet consectetur adipisicing elit.
               Praesentium dignissimos possimus molestiae corporis quod illum
