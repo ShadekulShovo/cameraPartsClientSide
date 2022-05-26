983bd26476791b4098055554dedf275173ea6cfb
Shadekul Shovo <shadekuljamalpur.bd21@gmail.com>
2022-05-26 18:19:50 +0600

    Fix

--- src/Pages/Dashboard/ManageServices.js
+++ src/Pages/Dashboard/ManageServices.js
@@ -1,60 +1,41 @@
-import React from 'react';
-import useServices from '../../hooks/useServices';
-import ManageServicesCard from './ManageServicesCard';
+import React from "react";
+import useServices from "../../hooks/useServices";
+import ManageServicesCard from "./ManageServicesCard";
 
 const ManageServices = () => {
-    const [services, setServices] = useServices();
-    return (
-        <div>
-            <h1 className='text-2xl font-mono font-bold text-center mb-3 text-secondary'>Manage services</h1>
+  const [services, setServices] = useServices();
+  return (
+    <div>
+      <h1 className="text-2xl font-mono font-bold text-center mb-3 text-secondary">
+        Manage Services
+      </h1>
 
-
-
-
-
-
-            <div class="overflow-x-auto">
-                <table class="table w-full">
-                    <thead>
-                        <tr>
-                            <th>S.No</th>
-                            <th>Product Name </th>
-                            <th>Price</th>
-                            <th>Order Quantity</th>
-                            <th>Available Quantity</th>
-                            <th>Remove Items</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-
-                        
-                            {
-                                services.map((service,index) => <ManageServicesCard
-
-                                    key={service._id}
-                                    service={service}
-                                      setServices={setServices}
-                                      
-                                    index={index}
-                                ></ManageServicesCard>)
-                            }
-                    
-
-                    </tbody>
-                </table>
-            </div>
-
-
-
-
-
-
-
-
-
-
-        </div>
-    );
+      <div class="overflow-x-auto">
+        <table class="table w-full">
+          <thead>
+            <tr>
+              <th>Serial No.</th>
+              <th>Product Name </th>
+              <th>Price</th>
+              <th>Order Quantity</th>
+              <th>Available Quantity</th>
+              <th>Remove Items</th>
+            </tr>
+          </thead>
+          <tbody>
+            {services.map((service, index) => (
+              <ManageServicesCard
+                key={service._id}
+                service={service}
+                setServices={setServices}
+                index={index}
+              ></ManageServicesCard>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default ManageServices;
