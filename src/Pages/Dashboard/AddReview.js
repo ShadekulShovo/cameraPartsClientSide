1c8731d571513952d70f01cc9c6cdbfee6b7fe84
Shadekul Shovo <shadekuljamalpur.bd21@gmail.com>
2022-05-26 18:26:26 +0600

    Fix

--- src/Pages/Dashboard/AddReview.js
+++ src/Pages/Dashboard/AddReview.js
@@ -1,86 +1,85 @@
-import React from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import auth from '../../firebase.init';
+import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "../../firebase.init";
 
 const AddReview = () => {
-    const [user, loading, error] = useAuthState(auth);
+  const [user, loading, error] = useAuthState(auth);
 
-    const handalReview = event => {
-        event.preventDefault();
-        const name = user.displayName
-        const comment = event.target.comment.value;
-        const rating = event.target.rating.value;
-        const email = user.email;
+  const handalReview = (event) => {
+    event.preventDefault();
+    const name = user.displayName;
+    const comment = event.target.comment.value;
+    const rating = event.target.rating.value;
+    const email = user.email;
 
-        const review = { name, comment, rating, email };
+    const review = { name, comment, rating, email };
 
-        fetch('http://localhost:8000/review', {
-            method: 'POST',
-            headers: {
-                'content-type': 'application/json',
-                authorization: `Bearer ${localStorage.getItem('accessToken')}`
-            },
-            body: JSON.stringify(review)
-        })
-            .then(res => res.json())
-            .then(data => {
-                console.log(data)
-                event.target.reset()
-            })
+    fetch("http://localhost:8000/review", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+      },
+      body: JSON.stringify(review),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        event.target.reset();
+      });
+  };
+  return (
+    <div>
+      <h2 className="text-2xl font-mono font-bold text-center mb-3 text-secondary uppercase">
+        {" "}
+        Write A Review
+      </h2>
 
+      <div class="card w-96 bg-base-100 shadow-xl mx-auto">
+        <div class="card-body">
+          <form
+            action=""
+            onSubmit={handalReview}
+            className="grid grid-cols-1 gap-3   mt-2"
+          >
+            <label htmlFor="user_name"> User Name</label>
+            <input
+              type="text"
+              name="name"
+              disabled
+              value={user?.displayName}
+              class="input input-bordered w-full max-w-xs"
+            />
 
-    }
-    return (
-        <div>
-            <h2 className='text-2xl font-mono font-bold text-center mb-3 text-secondary uppercase'> Add New Review</h2>
+            <label htmlFor="user_name">Comment</label>
+            <input
+              type="text"
+              name="comment"
+              class="input input-bordered w-full max-w-xs"
+            />
 
-            <div class="card w-96 bg-base-100 shadow-xl mx-auto">
-                <div class="card-body">
-                    
+            <label htmlFor="order_Quantity">Rating </label>
+            <input
+              type="number"
+              name="rating"
+              class="input input-bordered w-full max-w-xs"
+            />
 
-                    <form action='' onSubmit={handalReview} className='grid grid-cols-1 gap-3   mt-2'>
-                        <label htmlFor="user_name"> User Name</label>
-                        <input type="text" name='name' disabled value={user?.displayName} class="input input-bordered w-full max-w-xs" />
+            <label htmlFor="user_email"> User Email</label>
+            <input
+              type="email"
+              name="email"
+              disabled
+              value={user?.email}
+              class="input input-bordered w-full max-w-xs"
+            />
 
-                        <label htmlFor="user_name">comment</label>
-                        <input type="text" name='comment' class="input input-bordered w-full max-w-xs" />
-
-                        <label htmlFor="order_Quantity">Ratting </label>
-                        <input type="number" name='rating' class="input input-bordered w-full max-w-xs" />
-
-                        <label htmlFor="user_email"> User Email</label>
-                        <input type="email" name='email' disabled value={user?.email} class="input input-bordered w-full max-w-xs" />
-
-
-
-
-                        <input type="submit" class="btn btn-primary w-full max-w-xs" />
-
-                    </form>
-
-                </div>
-            </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            <input type="submit" class="btn btn-primary w-full max-w-xs" />
+          </form>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default AddReview;
