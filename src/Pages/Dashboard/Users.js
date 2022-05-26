12c74787e8cb033d880805b233d7636b0231a247
Shadekul Shovo <shadekuljamalpur.bd21@gmail.com>
2022-05-26 17:58:53 +0600

    Fix

--- src/Pages/Dashboard/Users.js
+++ src/Pages/Dashboard/Users.js
@@ -1,51 +1,54 @@
-import React from 'react';
-import { useQuery } from 'react-query';
-import Loading from '../Shared/Loading';
-import UserRow from './UserRow';
+import React from "react";
+import { useQuery } from "react-query";
+import Loading from "../Shared/Loading";
+import UserRow from "./UserRow";
 
 const Users = () => {
-    const { data: users, isLoading, refetch } = useQuery('users', () => fetch('http://localhost:8000/user',{
-        method: 'GET',
-        headers:{
-            authorization: `Bearer ${localStorage.getItem('accessToken')}`
-        }
-    }).then(res => res.json()));
+  const {
+    data: users,
+    isLoading,
+    refetch,
+  } = useQuery("users", () =>
+    fetch("http://localhost:8000/user", {
+      method: "GET",
+      headers: {
+        authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+      },
+    }).then((res) => res.json())
+  );
 
-    if (isLoading) {
-        return <Loading></Loading>
-    }
+  if (isLoading) {
+    return <Loading></Loading>;
+  }
 
-    return (
-        <div>
-            <h1 className='text-2xl font-mono font-bold text-center mb-3 text-secondary'>All of User :{users.length} </h1>
-            <div class="overflow-x-auto">
-                <table class="table w-full">
-                   
-                    <thead>
-                        <tr>
-                            <th>S.No</th>
-                            <th>User Email</th>
-                            <th>Make Admin</th>
-                            
-                        </tr>
-                    </thead>
-                    <tbody>
-
-                     {
-                         users.map((user,index)  =><UserRow
-                         key={user._id}
-
-                         user={user}
-                         refetch={refetch}
-                         index={index}
-                         ></UserRow>)
-                     }
-
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <h1 className="text-2xl font-mono font-bold text-center mb-3 text-secondary">
+        All of User :{users.length}{" "}
+      </h1>
+      <div class="overflow-x-auto">
+        <table class="table w-full">
+          <thead>
+            <tr>
+              <th>Serial No.</th>
+              <th>User E-mail</th>
+              <th>Make Admin</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user, index) => (
+              <UserRow
+                key={user._id}
+                user={user}
+                refetch={refetch}
+                index={index}
+              ></UserRow>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default Users;
