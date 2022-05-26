4b92f6fe4a2dd82e70968b7499109884c0517629
Shadekul Shovo <shadekuljamalpur.bd21@gmail.com>
2022-05-26 17:38:42 +0600

    Fix

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -7,15 +7,14 @@ const Banner = () => {
         <div class="hero-content flex-col lg:flex-row-reverse">
           <img src="https://i.ibb.co/XJK5tC1/pexels-pixabay-51383-removebg-preview.png" />
           <div className=" max-w-3xl">
-            <h1 class="text-5xl font-bold">Camera Parts </h1>
+            <h1 class="text-5xl font-bold">Camera Parts </h1> <br />
             <h2 class="text-3xl">For All Of Photography Enthusiast.</h2>
             <h3 class="text-2xl font-bold">Choose Which PARTS you NEED!!!</h3>
-
             <p class="py-6">
               Show off your unique style with the best lenses and camera parts
               created by featured artists and influencers!!!
             </p>
-            <button class="btn btn-primary">Get an Appointment</button>
+            <button class="btn btn-primary">Learn More</button>
           </div>
         </div>
       </div>
